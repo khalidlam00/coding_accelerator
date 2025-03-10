16927208e4be439641fcfd60c0a52f80fa6b49df
khalid <lamlahikhalid.com>
2025-03-10 14:13:57 +0100

    Ajout du fichier terre00.py avec l'affichage de l'alphabet

---
+++ terre00.py
@@ -0,0 +1,9 @@
+def afficher_alphabet():
+    # Utilisation d'une boucle pour afficher l'alphabet
+    for lettre in "abcdefghijklmnopqrstuvwxyz":
+        print(lettre, end='')
+    # Ajouter un retour à la ligne à la fin
+    print()
+
+# Appel de la fonction pour exécuter le programme
+afficher_alphabet() 
