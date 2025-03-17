73ff0deec7e734b1519d65bef768ca603d75ca94
khalidlam00 <122030839+khalidlam00@users.noreply.github.com>
2025-03-17 15:31:05 +0100

    Add files via upload

---
+++ terre14.py
@@ -0,0 +1,7 @@
+def celebrer_victoire():
+    adj = "cool" 
+    message = f"j'ai fini l'épreuve de la Terre c'était cool." 
+    print(message)
+
+if __name__ == "__main__":
+    celebrer_victoire()    
